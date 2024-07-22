81e5c982b84e639f5a811788a272cc116f80ef82
RuanPablo-7 <ToyaMON777@outlook.com>
2024-07-22 15:10:52 -0300

    atualização

--- previsão do tempo/script.js
+++ previsão do tempo/script.js
@@ -2,78 +2,68 @@
 let button = document.getElementById("button")
 let search = document.getElementById("search")
 
-/*
-import { createClient } from 'pexels';
+let random = Math.floor(Math.random()*8)
+console.log(random)
 
-const client = createClient('   ps8Ix62YhT5nyhokyO1uHIBDWA2D97p7PWGuIhKuEKihpWzV80kg45Mv');
-const query = 'Nature';
-
-client.photos.search({ query, per_page: 1 }).then(photos => photos.json())
-.then(dat => {
-    console.log(dat)
-})
-*/
 button.addEventListener("click", () => {
     console.log(search.value)
+    buscarclima()
+})
+
+addEventListener("keydown", (e) => {
+    //console.log(search.value)
     
+    if (e.key == "Enter") {
+        buscarclima()
+    }
+})
 
-    let key = "0de776d455fb8bc4ef0467a2ebf5dc57"
-   
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=${key}&units=metric&lang=pt_br`)
+function buscarclima(){
+    const key3 = "I5Eam4abFvgdPJ2LF--MZRukOMCYA3tGucJO1VFpAWk"
+    fetch(`https://api.unsplash.com/search/photos?query=${search.value}&client_id=${key3}`)
     .then(response => response.json())
-    .then(data=> {
-
-       
+    .then(dat => {
+            let img = dat.results[random].urls.full
+            document.body.style.backgroundImage = `url(${img})`;
+            console.log(dat)
+        })
         
-        document.getElementById("city").innerText = data.name + ", " + data.sys.country
-        document.getElementById("temp").innerText = data.main.temp + " °C"
-        document.getElementById("img").innerText = data.weather.icon
-        document.getElementById("stage").innerText = data.weather[0].description
-        console.log(data)
         
-
-
-    }).catch(error => {
-        document.getElementById("city").innerText = "Cidade não encontrada"
-        console.log("error", error)
-    })
+        let key2 = "0de776d455fb8bc4ef0467a2ebf5dc57"
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=${key2}&units=metric&lang=pt_br`)
+        .then(response => response.json())
+        .then(data => {   
+            document.getElementById("city").innerText = data.name + ", " + data.sys.country
+            document.getElementById("temp").innerText = data.main.temp + " °C"
+            document.getElementById("img").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+            document.getElementById("stage").innerText = data.weather[0].description
+            console.log(data)
+        }).catch(error => {
+            document.getElementById("city").innerText = "Cidade não encontrada"
+            console.log("error", error)
+        })
 
     search.value = ""
     
-})
-addEventListener("keydown", (e) => {
-    console.log(search.value)
 
-    if(e.key == "Enter"){
+
+}
 
         
         
-            let key = "0de776d455fb8bc4ef0467a2ebf5dc57"
-           
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=${key}&units=metric&lang=pt_br`)
-            .then(response => response.json())
-            .then(data=> {
         
-               
-                
-                document.getElementById("city").innerText = data.name + ", " + data.sys.country
-                document.getElementById("temp").innerText = data.main.temp + " °C"
-                document.getElementById("img").innerText = data.weather.icon
-                document.getElementById("stage").innerText = data.weather[0].description
-                console.log(data)
-                
-        
-        
-            }).catch(error => {
-                document.getElementById("city").innerText = "Cidade não encontrada"
-                console.log("error", error)
-            })
-        
-            search.value = ""
-    }
+  
+    /*
+    import { createClient } from 'pexels';
     
-})
-
+    const client = createClient('   ps8Ix62YhT5nyhokyO1uHIBDWA2D97p7PWGuIhKuEKihpWzV80kg45Mv');
+    const query = 'Nature';
+    
+    client.photos.search({ query, per_page: 1 }).then(photos => photos.json())
+    .then(dat => {
+        console.log(dat)
+    })
+    */
 
 /*
 let key2 = 
